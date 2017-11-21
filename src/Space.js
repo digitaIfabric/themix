3b9fb434caee48edf7d647b80bf8eb108f0b2605
digitalfabric92 <dwawryko@gmail.com>
2017-11-21 08:31:02 -0500

    Spotify player embedded

--- src/Space.js
+++ src/Space.js
@@ -44,14 +44,14 @@ class Space extends Component {
   render() {
     return (
         <aside className="messenger">
+          <iframe className="spotify" src="https://open.spotify.com/embed?uri=spotify:user:digital-fabric:playlist:2M3kZY1t1vocCub719j2qR&view=coverart"
+                  width="655" height="725" frameborder="0" allowtransparency="true"></iframe>
           <nav className="navbar">
             <a href="/" className="navbar-brand">The Mixer</a>
           </nav>
           <button id="showchat">Click to show chat</button>
           <MessageList messages={this.state.messages} />
           <ChatBar pressKey={this.handleKeyPress} className="chatbarclass"/>
-        <iframe src="https://open.spotify.com/embed?uri=spotify:user:digital-fabric:playlist:1xjOLosU4CWy8M4BSlAeqJ&view=coverart"
-          width="675" height="600" frameborder="0" allowtransparency="true"></iframe>
         </aside>
 
   );
