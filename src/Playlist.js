94f336b450e2e626204427f6b7bdc90604d5f6ec
digitalfabric92 <dwawryko@gmail.com>
2017-11-21 13:26:57 -0500

    Updated styles and added post request [bug]

--- src/Playlist.js
+++ src/Playlist.js
@@ -25,16 +25,46 @@ class Playlist extends Component {
             })
     }
 
-    // Request for the playlists tracks
+    // Request for the playlists tracks and post to the mix playlists
     getTracks = (playlistID) => {
         const accessToken = this.state.accessToken.access_token;
         const options = {headers: {'Authorization': 'Bearer ' + accessToken}, json: true}
-        let tracks = axios.get(`https://api.spotify.com/v1/users/digital-fabric/playlists/${playlistID}/tracks`, options)
+        axios.get(`https://api.spotify.com/v1/users/digital-fabric/playlists/${playlistID}/tracks`, options)
             .then((res) => {
                 console.log(res.data.items);
                 this.setState({tracks: res.data.items})
             })
+
+
+
+
+
+        //postTracks = () =>
+        // const options =
+        // axios.post(https://api.spotify.com/v1/users/digital-fabric/playlists/2M3kZY1t1vocCub719j2qR/tracks?position=5&uris=spotify%3Atrack%3A6X5OFBbrsHRsyO1zP7udgr`,options)
+        //     .then((res) => {
+        //         console.log(res.data.items);
+        //         this.setState({tracks: res.data.items})
+        //     })
+        // this.state.tracks[0].uri
+
+
     }
+
+    postTracks = () => {
+        const accessToken = this.state.accessToken.access_token;
+        const accessToken2 = 'BQCXSOOmIHi8ulBgII9SqCHXGwkI2uOmd8nfl4myBb8kSTOniM5vlTbCwiv2h66emCpZpwb_cFMvN-5YCQY2PGOKhJzJxPwDruGe-RM6QT0gyWimZPvEXPcTv5PqI4-JhkByO4lXTXS5rWqPRYlHIg4e9HdwUJpVuXD7FX4lr3IfHtq1JeY5t_UqZdRRb4saOtriqH2mu_cliCk55CNEgjTzCOCia4_Uu9NdMq1UOy5gxk_dtDU9hpox-kNIUd5It346VGfo9dl';
+        const uri = 'spotify%3Atrack%3A22nyEAEM29tcBRhukR089b';
+        // const options = {headers: {'Authorization': 'Bearer ' + accessToken}, json: true}
+        axios.post(`https://api.spotify.com/v1/users/digital-fabric/playlists/2M3kZY1t1vocCub719j2qR/tracks?uris=${uri}`, {headers: {'Authorization': 'Bearer ' + accessToken2}, json: true})
+            .then((res) => {
+                console.log('posted to playlist')
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
+
 
     render() {
         // This if handles the empty array of playlists at first
@@ -48,7 +78,8 @@ class Playlist extends Component {
                             <div id="card" key={index}>
                               <h1>{playlist.name}</h1>
                               <h3>{playlist.tracks.total} Tracks</h3>
-                              <button onClick={(evt) => this.getTracks(playlist.id)}>Add to Mix</button>
+                              <button onClick={(evt) => this.getTracks(playlist.id)}>Get Track Info</button>
+                              <button onClick={(evt) => this.postTracks()}>Add To Mix</button>
                             </div>
                         )})}
                 </div>
